cd7c9d3f2e6f7c0afff99fb17fe9cd0c56e9f854
Himanshu sangwan <pahimanshusangwan@gmail.com>
2024-07-21 19:27:28 +0530

    Your commit message

--- 2/src/components/Product-list/Productlist.jsx
+++ 2/src/components/Product-list/Productlist.jsx
@@ -3,13 +3,14 @@ import Productitem from "./compo/Productitem"
 function B1(){
     return <button>Click1</button>
 }
-
+const flag = false;
 
 
 function Productlist({name,city,d})
 {
-    return <div>{name}
-    {city   }
+    return <div>{flag?(<h1>{name}</h1>): ( <h1>{city }</h1>)}
+    {name}
+    {city }
     <ul>
         {d.map((single,index)=><Productitem a = {single}/>)}
 
--- 2/src/components/Product-list/compo/Productitem.jsx
+++ 2/src/components/Product-list/compo/Productitem.jsx
@@ -1,9 +1,9 @@
 function B1(){
-    return <button>Click</button>
+    return <button className="bg-red-600 text-3xl font-bold underline">Click</button>
 }
 
 function Productitem({a}){
-return<div>
+return<div className="bg-green-400 text-3xl font-bold underline">
     {a}
     <B1/>
 </div>
