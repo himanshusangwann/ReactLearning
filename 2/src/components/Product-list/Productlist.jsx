18e2021c87a7744d22650829d04e846936d64ebf
Himanshu sangwan <pahimanshusangwan@gmail.com>
2024-07-25 07:50:56 +0530

    usestate

--- 2/src/components/Product-list/Productlist.jsx
+++ 2/src/components/Product-list/Productlist.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import Productitem from "./compo/Productitem"
 
 function B1(){
     return <button>Click1</button>
 }
-const flag = false;
+const flag = true;
+
 
 
 function Productlist({name,city,d})
 {
+    const [state,setstate] = useState(flag)
+    const ToggleText=()=>{
+        setstate(!state)
+    }
     return <div>{flag?(<h1>{name}</h1>): ( <h1>{city }</h1>)}
     {name}
-    {city }
+    {city}
     <ul>
         {d.map((single,index)=><Productitem a = {single}/>)}
 
-    </ul>
+    </ul> 
 
-
+    {state?(<h1>{name}</h1>):(<h1>hello</h1>)}
+    <button onClick={ToggleText} className="font-bold ">Toggle Text</button>
     
     
     </div>
